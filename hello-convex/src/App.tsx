bb8b22695749edffd2aa95df557fbcd3f53868ef
James Cowling <james@zerowatt.io>
2021-08-25 18:55:01 -0700

    comments

--- hello-convex/src/App.tsx
+++ hello-convex/src/App.tsx
@@ -7,11 +7,15 @@ function Counter() {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
+    // Watch the results of the Convex function `getCounter` and call
+    // `setCounter` on the return value.
     return convex.query("getCounter").watch({}, setCounter);
   }, []);
 
   function incrementCounter(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
+    // Execute the Convex function `updateCounter` as a transaction that updates
+    // the counter value.
     convex.transaction("updateCounter").call();
   }
 
