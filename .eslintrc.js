5b5a1e7421532fbdc265092c7f77cabe5b0554d2
James Cowling <james@convex.dev>
2022-02-24 02:31:13 +0000

    Release 0.0.51
    384e1915257d907b24664c9ee9fb9a2abaeb05f0

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "none", // to demonstrate Netflify functions
+      },
+    ],
+  },
+};
