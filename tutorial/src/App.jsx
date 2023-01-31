e341dd0bd9e5645adf3e6ba9f53e68bfece55aea
Mike Wheaton <mike@wheaton.dev>
2023-01-31 13:44:18 -0600

    Update tutorial styles, ensure latest message is scrolled into view

--- tutorial/src/App.jsx
+++ tutorial/src/App.jsx
@@ -1,41 +1,46 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMutation, useQuery } from "../convex/_generated/react";
 
 export default function App() {
   const messages = useQuery("listMessages") || [];
-
-  const [newMessageText, setNewMessageText] = useState("");
   const sendMessage = useMutation("sendMessage");
-
+  const [newMessageText, setNewMessageText] = useState("");
   const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
+  const messagesList = useRef();
+
   async function handleSendMessage(event) {
     event.preventDefault();
     setNewMessageText("");
     await sendMessage(newMessageText, name);
   }
+
+  useEffect(() => {
+    messagesList.current.scrollTop = messagesList.current.scrollHeight;
+  }, [messages]);
+
   return (
-    <main>
-      <h1>Convex Chat</h1>
-      <p className="badge">
-        <span>{name}</span>
-      </p>
-      <ul>
-        {messages.map(message => (
-          <li key={message._id.toString()}>
-            <span>{message.author}:</span>
-            <span>{message.body}</span>
-            <span>{new Date(message._creationTime).toLocaleTimeString()}</span>
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={handleSendMessage}>
-        <input
-          value={newMessageText}
-          onChange={event => setNewMessageText(event.target.value)}
-          placeholder="Write a message…"
-        />
-        <input type="submit" value="Send" disabled={!newMessageText} />
-      </form>
-    </main>
+      <main>
+        <header>
+          <h1>Convex Chat</h1>
+          <span>{name}</span>
+        </header>
+        <ul ref={messagesList}>
+          {messages.map(message => (
+            <li key={message._id.toString()} className={message.author === name ? 'sent' : 'received'}>
+              <p>{message.body}</p>
+              <span>{message.author}{message.author === name && " (You)"} &bull; {new Date(message._creationTime).toLocaleTimeString()}</span>
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={handleSendMessage}>
+          <input
+            type="text"
+            value={newMessageText}
+            onChange={event => setNewMessageText(event.target.value)}
+            placeholder="Write a message…"
+          />
+          <button type="submit" disabled={!newMessageText}>Send</button>
+        </form>
+      </main>
   );
 }
