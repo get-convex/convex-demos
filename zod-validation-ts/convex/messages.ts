4962a31e6e5d884304ff97d093bea3783adff6e0
Tom Ballinger <tom@convex.dev>
2024-02-07 15:43:38 -0800

    Release 1.9.0
    fdd1f0485af5cbc4cd59a0eea97dfa650c9cc051

--- zod-validation-ts/convex/messages.ts
+++ zod-validation-ts/convex/messages.ts
@@ -1,6 +1,7 @@
-import { mutationWithZod, queryWithZod } from "./lib/withZod";
+import { mutationWithZod, queryWithZod } from "./lib/zod";
 import { z } from "zod";
 import { Doc } from "./_generated/dataModel";
+import { withSystemFields } from "convex-helpers/server/zod";
 
 export const send = mutationWithZod({
   args: { body: z.string(), author: z.string() },
@@ -14,4 +15,10 @@ export const list = queryWithZod({
   handler: async (ctx): Promise<Doc<"messages">[]> => {
     return await ctx.db.query("messages").collect();
   },
+  // Output validation is optional
+  output: z.array(
+    z.object(
+      withSystemFields("messages", { body: z.string(), author: z.string() })
+    )
+  ),
 });
