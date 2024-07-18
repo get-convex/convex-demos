e30b9751f2f5c60fc2eecea3f67a4e0924470cab
Tom Ballinger <tom@convex.dev>
2024-07-17 17:16:07 -0700

    Release 1.13.1
    9d0d96af4b1c3d4dcc32ede9939a29ef5316fd22

---
+++ react-query/src/App.tsx
@@ -0,0 +1,59 @@
+import { FormEvent, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { convexQuery, useConvexMutation } from "@convex-dev/convex-react-query";
+import { api } from "../convex/_generated/api";
+
+export default function App() {
+  const { data, error, isPending } = useQuery(
+    convexQuery(api.messages.list, {}),
+  );
+
+  const [newMessageText, setNewMessageText] = useState("");
+  const { mutate, isPending: sending } = useMutation({
+    mutationFn: useConvexMutation(api.messages.send),
+  });
+
+  const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
+  async function handleSendMessage(event: FormEvent) {
+    event.preventDefault();
+    if (!sending && newMessageText) {
+      mutate(
+        { body: newMessageText, author: name },
+        {
+          onSuccess: () => setNewMessageText(""),
+        },
+      );
+    }
+  }
+  if (error) {
+    return <div>Error: {error.toString()}</div>;
+  }
+  if (isPending) {
+    return <div>loading...</div>;
+  }
+  return (
+    <main>
+      <h1>Convex Chat</h1>
+      <p className="badge">
+        <span>{name}</span>
+      </p>
+      <ul>
+        {data.map((message) => (
+          <li key={message._id}>
+            <span>{message.author}:</span>
+            <span>{message.body}</span>
+            <span>{new Date(message._creationTime).toLocaleTimeString()}</span>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSendMessage}>
+        <input
+          value={newMessageText}
+          onChange={(event) => setNewMessageText(event.target.value)}
+          placeholder="Write a message…"
+        />
+        <input type="submit" value="Send" />
+      </form>
+    </main>
+  );
+}
